370ba8978c5b2cb4bb6af3da768c4762f8f17665
Alysia Petti <alysiapetti@gmail.com>
2019-10-16 10:57:48 -0700

    Create reportingForDuty.js

---
+++ reportingForDuty.js
@@ -0,0 +1,6 @@
+// Takes two string parameters and returns into following format: 'rank lastName reporting for duty!'
+reportingForDuty = (rank,lastName) => {
+  return `${rank} ${lastName} reporting for duty!`;
+}
+
+console.log(reportingForDuty('Private', 'Fido')) 

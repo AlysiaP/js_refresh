cb596f4664e019e8cf1387638837e5b802daa4dc
Alysia Petti <alysiapetti@gmail.com>
2019-11-06 10:00:56 -0800

    Create waterIntake.js

---
+++ waterIntake.js
@@ -0,0 +1,27 @@
+// forumla to determine how much water you should intake a day:
+
+// weight in pounds / 2.2 = X
+
+// X * age dependent
+//     If you're younger than 30, multiply by 40
+//     if you're between 30-55, multiply by 35
+//     if you're older than 55, multiply by 30
+
+// divide that sum by 28.3
+
+// your total is how many ounces of water you should drink each day. divide that number by 8 to see your result in cups
+
+const age;
+const weight;
+const dailyOunces;
+
+drinkThisMuch = (weight, age, dailyOunces) => {
+    dailyOunces = weight / 2.2;
+
+    if(age >) {
+
+    } else if () {
+
+    }
+}
+
